e1128a3258288b1112feca999e00008e17b67010
Flor <cief.flor@gmail.com>
2022-11-08 16:49:04 +1300

    Add files via upload
    Name that Triangle - If Statement

--- main.py
+++ main.py
@@ -1,40 +1,17 @@
-from datetime import datetime
+# Name that Triangle (If Statement)
 
-score = 0
-start_time = datetime.today()
+side_a = int(input("Enter the length of the first side \n"))
+side_b = int(input("Enter the width of the second side \n"))
+side_c = int(input("Enter the height of the third side \n"))
 
-answer_one = (input("What is the capital of NZ?\n\n"))
+if side_a == side_b == side_c:
+    print("Equilateral")
 
-if answer_one.lower() == "wellington":
-    score = score + 1
-    print("That is correct. Your score is ", score, "\n\n")
-else:
-    print("Incorrect. Your score is still ", score, "\n\n")
+if side_a == side_b and side_a != side_c \
+    or side_a == side_c and side_a != side_b \
+    or side_b == side_c and side_b != side_a:
+    print("Isosceles")
 
-answer_two = (input("What is the Maori name for NZ?\n\n"))
-
-if answer_two.lower() == "aotearoa":
-    score = score + 1
-    print("That is correct. Your score is ", score, "\n\n")
-else:
-    print("Incorrect. Your score is still ", score, "\n\n")
-
-answer_three = (input("What is the name of the New Zealand Rugby team?\n\n"))
-
-if answer_three.lower() == "all blacks":
-    score = score + 1
-    print("That is correct. Your score is ", score, "\n\n")
-else:
-    print("Incorrect. Your score is still ", score, "\n\n")
-
-answer_four = (input("What are the official languages of NZ?\n\n"))
-
-if answer_four.lower() == "english, maori and NZ Sing Language":
-    score = score + 1
-    print("That is correct. Your score is ", score, "\n\n")
-else:
-    print("Incorrect. Your score is still ", score, "\n\n")
-
-print("Your final score is ", score, "\n")
-print("Total time: ", datetime.today() - start_time)
+if side_a != side_b != side_c:
+    print("Scalar")
 
